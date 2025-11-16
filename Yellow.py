132e2910da51de34ba0f00b1152743f551c6810b
Mike Goodspeed <mikegoodspeed@users.noreply.github.com>
2025-11-16 14:36:11 -0600

    changed colors of Menu, and title screens

--- Yellow.py
+++ Yellow.py
@@ -190,13 +190,14 @@ class TitleScreen(Screen):
 
     def handle_event(self, event: pygame.event.Event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                self.manager.switch("MenuScreen")
+            # any key moves to menu
+            self.manager.switch("MenuScreen")
 
     def render(self, surface: pygame.Surface):
-        background_color = pygame.Color('black')
+        extremely_dark_blue = (0, 10, 47)
+        background_color = pygame.Color(extremely_dark_blue)
         surface.fill(background_color)
-        title_text = "Yellow - Press any key"
+        title_text = "Yellow - Press Enter/Return Key"
         antialias = True
         text_color = (235, 200, 110)
         text = self.font.render(title_text, antialias, text_color)
@@ -225,16 +226,15 @@ class MenuScreen(Screen):
                     self.manager.quit()
 
     def render(self, surface):
-        background_color = (30, 30, 30)
         selected_option_color = (255, 255, 100)
         default_option_color = (200, 200, 200)
-
-        surface.fill(background_color)
+        extremely_dark_blue = (0, 10, 47)
+        surface.fill(extremely_dark_blue)
         surface_width, surface_height = surface.get_size()
         for i, option in enumerate(self.options):
             color = selected_option_color if i == self.selected else default_option_color
             text = self.font.render(option, True, color)
-            spacing = i * 80
+            spacing = i * 57.5
             center_position = (surface_width//2, surface_height//2 + spacing)
             rect = text.get_rect(center=center_position)
             surface.blit(text, rect)
